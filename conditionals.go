42ab51fbacf1e459979727d364eedb1433a01b73
Rydam <rid.18chauhan@gmail.com>
2026-07-02 17:55:43 +0700

    feature/conditionals: add conditionals example

---
+++ conditionals.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func showConditionals() {
+	score := 85
+
+	if score >= 90 {
+		fmt.Println("Grade: A")
+	} else if score >= 80 {
+		fmt.Println("Grade: B")
+	} else {
+		fmt.Println("Grade: C or below")
+	}
+}
