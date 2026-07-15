66c1da47d3147621c01289745e12c2ca614660df
Rydam <rid.18chauhan@gmail.com>
2026-07-15 22:52:16 +0700

    error-handling: add basic divide with error check

---
+++ errors.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func divide(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, errors.New("cannot divide by zero")
+	}
+	return a / b, nil
+}
+
+func showErrors() {
+	result, err := divide(10, 2)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Printf("10 / 2 = %.2f\n", result)
+	}
+}
