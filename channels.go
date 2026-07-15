e597cd05eca42426cb41438e74cb071b637f788b
Rydam <rid.18chauhan@gmail.com>
2026-07-15 22:55:06 +0700

    concurrency: upgrade to buffered channel with 3 senders

--- channels.go
+++ channels.go
@@ -2,13 +2,17 @@ package main
 
 import "fmt"
 
-func sendData(ch chan int) {
-	ch <- 42
+func sendData(ch chan int, val int) {
+	ch <- val
 }
 
 func showChannels() {
-	ch := make(chan int)
-	go sendData(ch)
-	val := <-ch
-	fmt.Println("Received:", val)
+	ch := make(chan int, 3)
+	go sendData(ch, 10)
+	go sendData(ch, 20)
+	go sendData(ch, 30)
+
+	for i := 0; i < 3; i++ {
+		fmt.Println("Received:", <-ch)
+	}
 }
