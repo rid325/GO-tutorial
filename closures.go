ba890ecea7a429db568611f453d5716c369fcb46
Rydam <rid.18chauhan@gmail.com>
2026-07-17 23:43:51 +0700

    closures: add makeAdder closure

--- closures.go
+++ closures.go
@@ -10,9 +10,19 @@ func makeCounter() func() int {
 	}
 }
 
+func makeAdder(x int) func(int) int {
+	return func(y int) int {
+		return x + y
+	}
+}
+
 func showClosures() {
 	counter := makeCounter()
 	fmt.Println(counter())
 	fmt.Println(counter())
 	fmt.Println(counter())
+
+	addFive := makeAdder(5)
+	fmt.Println(addFive(3))
+	fmt.Println(addFive(10))
 }
