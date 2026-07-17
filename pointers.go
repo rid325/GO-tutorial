f8c247a4a1cfb0d8921617310b3224cd114c1273
Rydam <rid.18chauhan@gmail.com>
2026-07-17 23:34:47 +0700

    pointers: add double function

--- pointers.go
+++ pointers.go
@@ -6,9 +6,15 @@ func increment(n *int) {
 	*n++
 }
 
+func double(n *int) {
+	*n *= 2
+}
+
 func showPointers() {
 	x := 10
 	fmt.Println("Before:", x)
 	increment(&x)
-	fmt.Println("After:", x)
+	fmt.Println("After increment:", x)
+	double(&x)
+	fmt.Println("After double:", x)
 }
